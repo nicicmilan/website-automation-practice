10c2bde1ad97811dfa96f8e63530afe8fc53f4ba
Milan Nicic <nicicmilan1@gmail.com>
2023-03-18 15:09:06 +0100

    Changing login implementation

--- auth.js
+++ auth.js
@@ -3,6 +3,7 @@ function authenticate(event) {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     if (username === 'admin' && password === 'admin') {
+      localStorage.setItem('authenticated', true);
       window.location.href = 'content.html';
     } else {
       alert('Incorrect username or password');
